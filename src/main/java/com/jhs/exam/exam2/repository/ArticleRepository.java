e2372dfafa2bbb8f77d38c3149604086b9d5991d
jhs512 <jangka512@gmail.com>
2021-07-09 13:31:11 +0900

    제목, 본문 검색 파라미터 기능

--- src/main/java/com/jhs/exam/exam2/repository/ArticleRepository.java
+++ src/main/java/com/jhs/exam/exam2/repository/ArticleRepository.java
@@ -16,22 +16,37 @@ public class ArticleRepository {
 		sql.append(", memberId = ?", memberId);
 		sql.append(", title = ?", title);
 		sql.append(", `body` = ?", body);
-		
+
 		int id = MysqlUtil.insert(sql);
-		
+
 		return id;
 	}
 
-	public List<Article> getForPrintArticles(int limitFrom, int limitTake) {
+	public List<Article> getForPrintArticles(String searchKeywordTypeCode, String searchKeyword, int limitFrom,
+			int limitTake) {
 		SecSql sql = new SecSql();
 		sql.append("SELECT A.*");
 		sql.append(", IFNULL(M.nickname, '삭제된회원') AS extra__writerName");
 		sql.append("FROM article AS A");
 		sql.append("LEFT JOIN member M");
 		sql.append("ON A.memberId = M.id");
+		sql.append("WHERE 1");
+
+		if (searchKeyword != null && searchKeyword.length() > 0) {
+			switch (searchKeywordTypeCode) {
+			case "body":
+				sql.append("AND A.body LIKE CONCAT('%', ?, '%')", searchKeyword);
+				break;
+			case "title":
+			default:
+				sql.append("AND A.title LIKE CONCAT('%', ?, '%')", searchKeyword);
+				break;
+			}
+		}
+
 		sql.append("ORDER BY A.id DESC");
 		sql.append("LIMIT ?, ?", limitFrom, limitTake);
-		
+
 		return MysqlUtil.selectRows(sql, Article.class);
 	}
 
@@ -40,7 +55,7 @@ public class ArticleRepository {
 		sql.append("SELECT A.*");
 		sql.append("FROM article AS A");
 		sql.append("WHERE id = ?", id);
-		
+
 		return MysqlUtil.selectRow(sql, Article.class);
 	}
 
@@ -48,7 +63,7 @@ public class ArticleRepository {
 		SecSql sql = new SecSql();
 		sql.append("DELETE FROM article");
 		sql.append("WHERE id = ?", id);
-		
+
 		return MysqlUtil.delete(sql);
 	}
 
@@ -56,25 +71,38 @@ public class ArticleRepository {
 		SecSql sql = new SecSql();
 		sql.append("UPDATE article");
 		sql.append("SET updateDate = NOW()");
-		
-		if ( title != null ) {
+
+		if (title != null) {
 			sql.append(", title = ?", title);
 		}
-		
-		if ( body != null ) {
+
+		if (body != null) {
 			sql.append(", body = ?", body);
 		}
-		
+
 		sql.append("WHERE id = ?", id);
-		
+
 		return MysqlUtil.update(sql);
 	}
 
-	public int getArticlesCount() {
+	public int getArticlesCount(String searchKeywordTypeCode, String searchKeyword) {
 		SecSql sql = new SecSql();
 		sql.append("SELECT COUNT(*) AS cnt");
 		sql.append("FROM article AS A");
-		
+		sql.append("WHERE 1");
+
+		if (searchKeyword != null && searchKeyword.length() > 0) {
+			switch (searchKeywordTypeCode) {
+			case "body":
+				sql.append("AND A.body LIKE CONCAT('%', ?, '%')", searchKeyword);
+				break;
+			case "title":
+			default:
+				sql.append("AND A.title LIKE CONCAT('%', ?, '%')", searchKeyword);
+				break;
+			}
+		}
+
 		return MysqlUtil.selectRowIntValue(sql);
 	}
 }
